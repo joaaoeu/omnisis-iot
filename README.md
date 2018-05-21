098397fb5023366d1f9f90ab10290e0e6509da1d
joaaoeu <hello@joaao.eu>
2018-05-21 05:42:51 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ominisis
+Projeto desenvolvido para trabalho interdisciplinar do 6° semestre do curso de Design Digital da Universidade Anhembi Morumbi.
+
+O projeto consiste em um protótipo de catraca em Arduino, onde é possível cadastrar uma TAG em um sistema através de comunicação do Arduino com API (desenvolvida em Node.js) por WiFi, e vincular a um usuário. Então a TAG é usada para controle de acesso, que confere na API se a TAG é válida e está com acesso para então decidir se libera ou não a catraca (simulada por um LED).
+
+Utilizamos um controlador com WiFi, leitor de RFID, Leds e um LCD para exibir mensagens.
+
+# License
+[WTFPL](http://www.wtfpl.net/)
---
+++ accessControl/accessControl.ino
@@ -0,0 +1,117 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define ledSimulation D8
+#define RST_PIN D3
+#define SS_PIN D4
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+const char* machine = "access01";
+const char* ssid = "ssid";
+const char* password = "password";
+const char* server = "http://000.000.00.000:0000/api/br/v1/modules/iot/cards/";
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledSimulation, OUTPUT);
+  digitalWrite(ledSimulation, LOW);
+  lcd.init();
+  lcd.backlight();
+  SPI.begin();
+  mfrc522.PCD_Init();
+
+  wifiConnect();
+}
+
+void wifiConnect() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("BUSCANDO REDE...");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+  }
+
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("CONECTADO...");
+  delay(2000);
+
+  lcd.clear();
+  lcd.setCursor(1,0);
+  lcd.print("PASSE O CARTAO");
+}
+
+void tagRead(MFRC522 mfrc522){
+  char tag[15];
+  char s[100];
+  for (int i = 0; i < mfrc522.uid.size; i++){
+    sprintf(s,"%x",mfrc522.uid.uidByte[i]);
+    strcat( &tag[i] , s);
+  }
+  serverConnect(tag);
+  return;
+}
+
+void serverConnect(String tag){
+  HTTPClient http;
+  http.begin(server + tag + "?private_key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX&machine=" + machine);
+  int httpCode = http.GET();
+
+  if (httpCode > 0) {
+    String resultJson = http.getString();
+    String line1 = resultJson.substring(10,26);
+    String line2 = resultJson.substring(37,53);
+    String access = resultJson.substring(65,66);
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(line1);
+    lcd.setCursor(0,1);
+    lcd.print(line2);
+
+    if(access == "1"){ digitalWrite(ledSimulation, HIGH); }
+
+  } else {
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("FALHA NA CONEXAO");
+      lcd.setCursor(0,1);
+      lcd.print("AO SERVIDOR...");
+  }
+
+  delay(5000);
+  lcd.clear();
+  lcd.setCursor(1,0);
+  lcd.print("PASSE O CARTAO");
+  digitalWrite(ledSimulation, LOW);
+  return;
+}
+
+void loop() {
+  if(WiFi.status() == WL_CONNECTED){
+
+    if ( ! mfrc522.PICC_IsNewCardPresent()) {
+      return;
+    }
+
+    if ( ! mfrc522.PICC_ReadCardSerial()) {
+      return;
+    }
+
+    tagRead(mfrc522);
+
+    delay(1000);
+
+  } else { wifiConnect(); }
+}
